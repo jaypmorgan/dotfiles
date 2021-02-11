1606d8bf7c574a7a4b8b45d20a2bb5e9ce936d34
Jay Morgan <jaymiles17@gmail.com>
2021-02-11 17:21:41 +0000

    Fix incorrect path in copy

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ FILES=(".emacs.d/init.el"
        ".emacs.d/config.org"
        ".xmonad/xmonad.hs");
 for FILE in "${FILES[@]}"; do
-    cp $FILE "$HOME/";
+    cp "$(basename -- $FILE)" "$HOME/$FILE";
 done
