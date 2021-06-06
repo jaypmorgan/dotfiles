70f2a99569725e556c66a09579d13ee528351944
Jay Morgan <jaymiles17@gmail.com>
2021-06-06 18:53:48 +0100

    Remove XMonad

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FILES=(".emacs.d/init.el"
-       ".emacs.d/config.org"
-       ".xmonad/xmonad.hs");
-for FILE in "${FILES[@]}"; do
-    cp "$(basename -- $FILE)" "$HOME/$FILE";
-done
