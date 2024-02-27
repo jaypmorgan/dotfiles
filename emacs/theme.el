bad00810716c92e57fa5ddcf49a99a7ad5469efa
Jay Morgan <jaymorgan@eng152-5cd20424mz.swan.ac.uk>
2024-02-27 09:37:49 +0000

    Change font only when it is found

--- emacs/theme.el
+++ emacs/theme.el
@@ -1,13 +1,13 @@
 ;; set the font -- only if the font is available though.
 (defun morg/configure-font-hook ()
   (let ((font-name "IBM Plex Mono")
-	(font-size 12))
-    ;; (when (find-font (font-spec :name font-name)))
-    (add-to-list 'default-frame-alist `(font . ,(format "%s-%s" font-name font-size)))
-    (set-face-attribute 'default nil :font font-name)
-    (set-frame-font (format "%s %d" font-name font-size) nil t)
-    (custom-set-faces `(default ((t (:inherit nil :height ,(* font-size 10) :family font-name)))))
-    (message "Set font to: %s" font-name)))
+        (font-size 12))
+    (when (find-font (font-spec :name font-name))
+      (add-to-list 'default-frame-alist `(font . ,(format "%s-%s" font-name font-size)))
+      (set-face-attribute 'default nil :font font-name)
+      (set-frame-font (format "%s %d" font-name font-size) nil t)
+      (custom-set-faces `(default ((t (:inherit nil :height ,(* font-size 10) :family font-name)))))
+      (message "Set font to: %s" font-name))))
 
 ;; apply the font face after each frame is created
 ;; this hook is necessary for when daemon mode is used.
