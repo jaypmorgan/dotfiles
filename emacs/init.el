61110a2058dc16bf50bcbd402ed8c7553d098a48
Jay Paul Morgan <jaymiles17@gmail.com>
2024-05-02 21:42:10 +0100

    Use menu bar in OSX

--- emacs/init.el
+++ emacs/init.el
@@ -4,8 +4,10 @@
 ;;--------------------------
 
 (tool-bar-mode -1)
-(menu-bar-mode -1)
 (scroll-bar-mode -1)
+;; menu bar for osx is fine.
+(when (not (eq system-type 'darwin))
+  (menu-bar-mode -1))
 
 ;; no splash screen
 (setq inhibit-splash-screen t)
